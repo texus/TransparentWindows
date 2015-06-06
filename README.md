fcccd2a4baddbc02c19dbdf51d792c639b8ee36e
Bruno Van de Velde <vdv_b@tgui.eu>
2015-06-07 00:26:48 +0200

    The window can now also be semi-transparent on linux

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
 Transparent Windows
 ===================
 
-This repository contains an example of how to create non-rectangular semi-transparent windows on multiple operating systems with SFML.
-It loads an image with transparent parts, uses this image to set the shape of the window and draws the image on the window.
+This repository contains an example of how to create non-rectangular semi-transparent windows on multiple operating systems with SFML (which is only used to load the image and display the result).
+It loads an image with transparent parts, uses this image to set the shape of the window and draws the image on the window. The code also contains an opacity variable to set the opacity of the window.
 
 Currently the code only works on Windows and Linux, but I hope to add OS X in the future.
-
-Currently the transparency only works on Windows, it is not yet implemented for Linux.
 
 Feel free to send pull requests to improve the code.
 
@@ -19,4 +17,4 @@ On Linux:
 
 On Windows:
 
-    You should link to sfml the way you do it for your own programs.
+    You should link to sfml the way you do it for your own programs. No special libraries are needed for the transparency.
--- Regions.cpp
+++ Regions.cpp
@@ -2,106 +2,124 @@
 
 #if defined (SFML_SYSTEM_WINDOWS)
     #include <windows.h>
+
+    bool setTransparent(HWND hWnd, const sf::Image& image, unsigned char alpha)
+    {
+        const sf::Uint8* pixelData = image.getPixelsPtr();
+        HRGN hRegion = CreateRectRgn(0, 0, image.getSize().x, image.getSize().y);
+
+        // Determine the visible region
+        for (unsigned int y = 0; y < image.getSize().y; y++)
+        {
+            for (unsigned int x = 0; x < image.getSize().x; x++)
+            {
+                if (pixelData[y * image.getSize().x * 4 + x * 4 + 3] == 0)
+                {
+                    HRGN hRegionDest = CreateRectRgn(0, 0, 1, 1);
+                    HRGN hRegionPixel = CreateRectRgn(x, y, x+1, y+1);
+                    CombineRgn(hRegionDest, hRegion, hRegionPixel, RGN_XOR);
+                    DeleteObject(hRegion);
+                    DeleteObject(hRegionPixel);
+                    hRegion = hRegionDest;
+                }
+            }
+        }
+
+        SetWindowRgn(hWnd, hRegion, true);
+        DeleteObject(hRegion);
+
+        // Set the transparency
+        SetWindowLong(hWnd, GWL_EXSTYLE, GetWindowLong(hWnd, GWL_EXSTYLE) | WS_EX_LAYERED);
+        SetLayeredWindowAttributes(hWnd, 0, alpha, LWA_ALPHA);
+        return true;
+    }
 #elif defined (SFML_SYSTEM_LINUX)
-    #include <X11/Xlib.h>
-    #include <X11/Xregion.h>
+    #include <X11/Xatom.h>
     #include <X11/extensions/shape.h>
-    #undef None
+
+    bool setTransparent(Window wnd, const sf::Image& image, unsigned char alpha)
+    {
+        const sf::Uint8* pixelData = image.getPixelsPtr();
+        Display* display = XOpenDisplay(NULL);
+
+        // Set the transparency
+        if (alpha != 255)
+        {
+            unsigned long opacity = (0xffffffff / 0xff) * alpha;
+            Atom property = XInternAtom(display, "_NET_WM_WINDOW_OPACITY", false);
+            if (property != None)
+                XChangeProperty(display, wnd, property, XA_CARDINAL, 32, PropModeReplace, (unsigned char*)&opacity, 1);
+        }
+
+        // Try to set the window shape
+        int event_base;
+        int error_base;
+        if (XShapeQueryExtension(display, &event_base, &error_base))
+        {
+            Pixmap pixmap = XCreatePixmap(display, wnd, image.getSize().x, image.getSize().y, 1);
+            GC gc = XCreateGC(display, pixmap, 0, NULL);
+
+            XSetForeground(display, gc, 1);
+            XFillRectangle(display, pixmap, gc, 0, 0, image.getSize().x, image.getSize().y);
+            XSetForeground(display, gc, 0);
+
+            for (unsigned int y = 0; y < image.getSize().y; y++)
+            {
+                for (unsigned int x = 0; x < image.getSize().x; x++)
+                {
+                    if (pixelData[y * image.getSize().x * 4 + x * 4 + 3] == 0)
+                        XFillRectangle(display, pixmap, gc, x, y, 1, 1);
+                }
+            }
+
+            XShapeCombineMask(display, wnd, ShapeBounding, 0, 0, pixmap, ShapeSet);
+            XFreePixmap(display, pixmap);
+            XFreeGC(display, gc);
+            XFlush(display);
+            return true;
+        }
+        else // Shape extension is not supported, window will not be of requested shape
+        {
+            XFlush(display);
+            return false;
+        }
+    }
+
+    #undef None // None conflicts with SFML
 #endif
 
 int main()
 {
-    unsigned char transparency = 150;
+    // Change this to the wanted transparency
+    const unsigned char opacity = 150;
 
+    // Load an image with transparent parts that will define the shape of the window
     sf::Image backgroundImage;
     backgroundImage.loadFromFile("image.png");
 
-    sf::RenderWindow window(sf::VideoMode(400, 300), "TGUI window", sf::Style::None);
+    // Create the window and center it on the screen
+    sf::RenderWindow window(sf::VideoMode(backgroundImage.getSize().x, backgroundImage.getSize().y, 32), "Transparent Window", sf::Style::None);
     window.setPosition(sf::Vector2i((sf::VideoMode::getDesktopMode().width - backgroundImage.getSize().x) / 2,
                                     (sf::VideoMode::getDesktopMode().height - backgroundImage.getSize().y) / 2));
 
+    if (!setTransparent(window.getSystemHandle(), backgroundImage, opacity))
+    {
+        // You can execute some code here on unsupported OS or when linux display doesn't support shape extention
+    }
+
+    // We will also draw the image on the window instead of just showing an empty window with the wanted shape
     sf::Texture backgroundTexture;
     sf::Sprite backgroundSprite;
     backgroundTexture.loadFromImage(backgroundImage);
     backgroundSprite.setTexture(backgroundTexture);
 
-    bool fallback = true;
-    const sf::Uint8* pixelData = backgroundImage.getPixelsPtr();
-
-#if defined (SFML_SYSTEM_WINDOWS)
-    fallback = false;
-
-    HWND hWnd = window.getSystemHandle();
-    HRGN hRegion = CreateRectRgn(0, 0, backgroundImage.getSize().x, backgroundImage.getSize().y);
-
-    for (unsigned int y = 0; y < backgroundImage.getSize().y; y++)
-    {
-        for (unsigned int x = 0; x < backgroundImage.getSize().x; x++)
-        {
-            if (pixelData[y * backgroundImage.getSize().x * 4 + x * 4 + 3] == 0)
-            {
-                HRGN hRegionDest = CreateRectRgn(0, 0, 1, 1);
-                HRGN hRegionPixel = CreateRectRgn(x, y, x+1, y+1);
-                CombineRgn(hRegionDest, hRegion, hRegionPixel, RGN_XOR);
-                DeleteObject(hRegion);
-                DeleteObject(hRegionPixel);
-                hRegion = hRegionDest;
-            }
-        }
-    }
-
-    SetWindowRgn(hWnd, hRegion, true);
-    DeleteObject(hRegion);
-
-    // Set the transparency
-    SetWindowLong(hWnd, GWL_EXSTYLE, GetWindowLong(hWnd, GWL_EXSTYLE) | WS_EX_LAYERED);
-    SetLayeredWindowAttributes(hWnd, 0, transparency, LWA_ALPHA);
-
-#elif defined (SFML_SYSTEM_LINUX)
-    Window wnd = window.getSystemHandle();
-    Display* display = XOpenDisplay(NULL);
-
-    int event_base;
-    int error_base;
-    if (XShapeQueryExtension(display, &event_base, &error_base))
-    {
-        fallback = false;
-
-        Pixmap pixmap = XCreatePixmap(display, wnd, backgroundImage.getSize().x, backgroundImage.getSize().y, 1);
-        GC gc = XCreateGC(display, pixmap, 0, NULL);
-
-        XSetForeground(display, gc, 1);
-        XFillRectangle(display, pixmap, gc, 0, 0, backgroundImage.getSize().x, backgroundImage.getSize().y);
-        XSetForeground(display, gc, 0);
-
-        for (unsigned int y = 0; y < backgroundImage.getSize().y; y++)
-        {
-            for (unsigned int x = 0; x < backgroundImage.getSize().x; x++)
-            {
-                if (pixelData[y * backgroundImage.getSize().x * 4 + x * 4 + 3] == 0)
-                    XFillRectangle(display, pixmap, gc, x, y, 1, 1);
-            }
-        }
-
-        XShapeCombineMask(display, wnd, ShapeBounding, 0, 0, pixmap, ShapeSet);
-        XFreePixmap(display, pixmap);
-        XFreeGC(display, gc);
-        XFlush(display);
-    }
-#endif
-
-    // Execute this code on unsupported OS or when linux display doesn't support shape extention
-    if (fallback)
-    {
-        // Should something happen when the window can't get the desired shape?
-    }
-
+    // Main loop to display the image while the window is open (pressing the escape key to close the window)
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            if (event.type == sf::Event::Closed || (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape))
                 window.close();
         }
 
